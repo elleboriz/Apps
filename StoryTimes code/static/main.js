1ac1f13aba7547e9afb65742a0751a734c56edae
Boris Jing <115875711+elleboriz@users.noreply.github.com>
2023-02-27 23:42:23 +0100

    Update main.js
    tooltips feature

--- StoryTimes code/static/main.js
+++ StoryTimes code/static/main.js
@@ -1,2 +1,3 @@
-console.log("worked")
-
+// tolltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
