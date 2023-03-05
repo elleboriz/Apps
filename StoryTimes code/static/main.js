f3dc761791a7454b0169b7bd6da1b18112bd750b
Boris Jing <115875711+elleboriz@users.noreply.github.com>
2023-03-05 01:03:31 +0100

    Delete StoryTimes code/static directory

--- StoryTimes code/static/main.js
+++
@@ -1,3 +0,0 @@
-// tolltips
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
