93c80b83bd2a08b8f0f3eed2e5e106ff77ffb4c9
Boris Jing <115875711+elleboriz@users.noreply.github.com>
2023-03-08 23:48:18 +0100

    Update models.py
    update User model

--- StoryTimes code/storyApp/models.py
+++ StoryTimes code/storyApp/models.py
@@ -1,12 +1,23 @@
 from storyApp import db
+from flask_bcrypt import Bcrypt
+
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key =True)
+    username = db.Column(db.String(length=15) , nullable=False,unique=True)
+    email_address = db.Column(db.String(length=45) , nullable=False,unique=True)
+    password_hash = db.Column(db.String(length=60) , nullable=False)
+    uploaded_stories = db.relationship("Story", backref="uploaded_by_user" , lazy=True)
 
 
 class Story(db.Model):
     id = db.Column(db.Integer(), primary_key =True)
+    uploaded_by = db.Column(db.Integer(), db.ForeignKey('user.id'))
     title = db.Column(db.String(length=80), nullable=False)
     category = db.Column(db.String(), nullable=False)
-    body = db.Column(db.String(), nullable=False , unique=True)
-    img = db.Column(db.LargeBinary)
+    description = db.Column(db.String(length=300) , nullable=True)
+    body = db.Column(db.String(length=200), nullable=False , unique=True)
+    img = db.Column(db.db.String())
     img_name = db.Column(db.String())
     mimetype = db.Column(db.String())
 
