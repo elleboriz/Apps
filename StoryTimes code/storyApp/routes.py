31bed82f505268e3035d13be58c55b51635650f1
Boris Jing <115875711+elleboriz@users.noreply.github.com>
2023-03-08 23:48:58 +0100

    Update routes.py
    register user

--- StoryTimes code/storyApp/routes.py
+++ StoryTimes code/storyApp/routes.py
@@ -1,7 +1,7 @@
-from storyApp import app
-from flask import render_template
-from storyApp.models import Story
-
+from storyApp import app , db
+from flask import render_template , request , flash ,redirect ,url_for
+from storyApp.models import Story ,User
+from storyApp.forms import RegistrationForm
 
 @app.route("/")
 @app.route("/homepage")
@@ -14,3 +14,16 @@ def StoryTimes_story():
 
     stories = Story.query.all()
     return render_template("stories.html",stories = stories)
+
+
+@app.route("/register" , methods=["GET", "POST"])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == "POST" and form.validate():
+        user = User(form.username.data , form.email.data ,form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash(f"welcome {user.username}" ," info")
+        return redirect(url_for('StoryTimes_story'),200)
+    else:
+        return render_template("register.html", form = form)
