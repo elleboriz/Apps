f91e792799d6cb3f4099cbc4209b0ca727c5b6ef
Boris Jing <115875711+elleboriz@users.noreply.github.com>
2023-03-16 23:33:02 +0100

    Update routes.py
    registration form

--- StoryTimes code/storyApp/routes.py
+++ StoryTimes code/storyApp/routes.py
@@ -1,29 +1,37 @@
 from storyApp import app , db
-from flask import render_template , request , flash ,redirect ,url_for
+from flask import render_template ,  flash ,redirect ,url_for
 from storyApp.models import Story ,User
 from storyApp.forms import RegistrationForm
 
 @app.route("/")
 @app.route("/homepage")
-def StoryTimes_home():
+def home_page():
     return render_template("home.html")
 
 
 @app.route("/stories")
-def StoryTimes_story():
+def story_page():
 
     stories = Story.query.all()
     return render_template("stories.html",stories = stories)
 
 
 @app.route("/register" , methods=["GET", "POST"])
-def register():
-    form = RegistrationForm(request.form)
-    if request.method == "POST" and form.validate():
-        user = User(form.username.data , form.email.data ,form.password.data)
-        db.session.add(user)
+def register_page():
+    form = RegistrationForm()
+    if form.validate_on_submit() :
+        new_user = User(
+            username = form.username.data ,
+            email_address = form.email.data ,
+            password = form.password.data
+        )
+
+        db.session.add(new_user)
         db.session.commit()
-        flash(f"welcome {user.username}" ," info")
-        return redirect(url_for('StoryTimes_story'),200)
-    else:
-        return render_template("register.html", form = form)
+        return redirect(url_for('story_page'))
+
+    if form.errors :
+        for error_msg in form.errors.values():
+            flash(f"{error_msg[0]}", category='danger')
+
+    return render_template("register.html", form = form)
