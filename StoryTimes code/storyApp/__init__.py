72df1e3647dc7a07c2bb06f60e6d6ddf5a2da593
Boris Jing <115875711+elleboriz@users.noreply.github.com>
2023-03-16 23:37:45 +0100

    Update __init__.py
    regitration form

--- StoryTimes code/storyApp/__init__.py
+++ StoryTimes code/storyApp/__init__.py
@@ -1,9 +1,13 @@
-from flask import Flask , render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
 app = Flask(__name__)
 app.app_context().push()
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///story_db.db"
+app.config['SECRET_KEY'] = '0fdee5a425d992541bf7265c'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
 from storyApp import routes
+
 
