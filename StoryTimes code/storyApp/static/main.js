d666d7a6205c69333077cd10d289a6f758c94de2
Boris Jing <115875711+elleboriz@users.noreply.github.com>
2023-03-25 19:11:50 +0100

    Update main.js
    updates js storytimes

--- StoryTimes code/storyApp/static/main.js
+++ StoryTimes code/storyApp/static/main.js
@@ -1,3 +1,41 @@
-// tolltips
-const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+// bootstrap tolltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+// bootstrap Toasts
+
+const toastElList = document.querySelector('.toast');
+if(toastElList){
+    const toast = new bootstrap.Toast(toastElList);
+    toast.show()
+}
+
+
+// bootstrap modals
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+if(myModal){
+    myModal.addEventListener('shown.bs.modal', () => {
+    myInput.focus()
+    })
+}
+
+console.log(true)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
