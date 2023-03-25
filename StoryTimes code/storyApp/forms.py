b018faa5ebf7568776d509b1b55c32f253879611
Boris Jing <115875711+elleboriz@users.noreply.github.com>
2023-03-25 19:10:04 +0100

    Update forms.py
    updates Storytimes

--- StoryTimes code/storyApp/forms.py
+++ StoryTimes code/storyApp/forms.py
@@ -1,4 +1,4 @@
-from wtforms import StringField ,PasswordField , SubmitField
+from wtforms import StringField ,PasswordField , SubmitField , TextAreaField ,SelectField ,FileField
 from wtforms.validators import DataRequired,Length , Email , EqualTo ,ValidationError
 from flask_wtf import FlaskForm
 from storyApp.models import User
@@ -7,10 +7,10 @@ from storyApp.models import User
 class RegistrationForm(FlaskForm):
 
 
-    username = StringField(label='Username' , validators = [Length(min=4 , max= 30,), DataRequired()])
-    email = StringField(label='Email Address',  validators = [Email() , DataRequired()])
-    password = PasswordField(label='New Password',validators =[Length(min=6) , DataRequired()])
-    password_confirm = PasswordField(label="Confirm Password" , validators=[DataRequired(),EqualTo("password" , message="Sorry Passwords don't match")])
+    username = StringField(label='Username *' , validators = [Length(min=4 , max= 30,), DataRequired()])
+    email = StringField(label='Email Address *',  validators = [Email() , DataRequired()])
+    password = PasswordField(label='New Password *',validators =[Length(min=6) , DataRequired()])
+    password_confirm = PasswordField(label="Confirm Password *" , validators=[DataRequired(),EqualTo("password" , message="Sorry Passwords don't match")])
     submit = SubmitField(label="Create account")
 
     def validate_username(self , username_to_check):
@@ -23,3 +23,23 @@ class RegistrationForm(FlaskForm):
         if email_address:
             raise ValidationError('Sorry!! Email address already exist, Please try a different email address')
 
+class LoginForm(FlaskForm):
+    username = StringField(label='Username *', validators=[Length(min=3 ,  message='username too short') , DataRequired()])
+    password = StringField(label='Password *' , validators=[Length(min=6, message='Password too short') , DataRequired()])
+    submit = SubmitField(label='Login')
+    
+ 
+class Add_storyForm(FlaskForm):
+    title = StringField(label="Title *" , validators=[Length(min=3 , max=30) ,DataRequired() ] ) 
+    description = TextAreaField(label="Description *" , validators=[DataRequired() ] ) 
+    body = TextAreaField(label="Body *" , validators=[DataRequired()] ) 
+    category = SelectField(label="Category *" , choices=['funny','wisdom','morals','love'],validators=[DataRequired()] ) 
+    image = FileField(label="Cover image [ optional ]" , validators=[DataRequired()] ) 
+    
+    submit = SubmitField(label='Add Story')
+    
+class LikeForm(FlaskForm):
+    submit = SubmitField(label='')
+
+class DeslikeForm(FlaskForm):
+    submit = SubmitField(label='') 
