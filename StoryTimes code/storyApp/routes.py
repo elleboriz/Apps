6eb909a22fe72730d891180f2b5530aba465197b
Boris Jing <115875711+elleboriz@users.noreply.github.com>
2023-04-04 22:53:27 +0100

    Update routes.py
    special function for Posting New Story

--- StoryTimes code/storyApp/routes.py
+++ StoryTimes code/storyApp/routes.py
@@ -1,16 +1,22 @@
-from storyApp import app , db ,ALLOWED_EXTENSIONS
+from storyApp import app , db 
 from flask import render_template ,  flash ,redirect ,url_for ,request 
-from flask_login import login_user , logout_user  , current_user 
+from flask_login import login_user , logout_user  
 from storyApp.models import Story ,User 
 from storyApp.forms import RegistrationForm , LoginForm ,LikeForm , DeslikeForm ,Add_storyForm
-from storyApp.special_func import SPECIAL_FUNC
-from werkzeug.utils import secure_filename
-from os import path
+from storyApp.special_func import  Route_func
 
 
 @app.route("/")
 def home_page():
-    return render_template("home.html")
+    
+    stories = Story.query.all()
+    addstory_form = Add_storyForm()
+    if request.method == "POST" :
+        if addstory_form.validate_on_submit() :
+            Route_func.add_story(addstory_form)
+            redirect(url_for('story_page'))
+
+    return render_template("home.html",stories = stories ,addstory_form=addstory_form)
 
 
 @app.route("/stories", methods=["GET", "POST"])
@@ -27,14 +33,10 @@ def story_page():
         pass
     if request.method == "POST" :
         if addstory_form.validate_on_submit() :
-            new_story = Story(title = addstory_form.title.data , category = addstory_form.category.data, description = addstory_form.description.data, body=addstory_form.body.data )
-            db.session.add(new_story)
-            db.commit()
-        
-        
-            
-            
-            redirect(url_for('story_page'))
+            Route_func.add_story(addstory_form)
+
+            flash('New Post Added', category='success')
+            return redirect(url_for('story_page'))
     
     
     stories = Story.query.all()
@@ -104,58 +106,12 @@ def logout():
 def addstory():
     stories = Story.query.all() 
     addstory_form = Add_storyForm()
-    
+      
     if request.method == 'POST' : 
-        if not current_user.is_authenticated:
-            flash('You are not Logged in , Login in Other to AddStory' , category='danger')
-            
-        
-        else:
-            file = request.files['image']
-            filestatus = True
-            story_title = addstory_form.title.data
-            if 'image' not in request.files or file.filename == '':
-                filestatus = False
-                flash('Story uploaded without cover image' , category='warning')
-                
-                
-            if SPECIAL_FUNC.allowed_file(file.filename) and filestatus :
-                filename = secure_filename(file.filename)
-                file.save(path.join(app.config['UPLOAD_FOLDER'], filename))          
-            
-                new_story = Story(title = addstory_form.title.data , category = addstory_form.category.data, description = addstory_form.description.data, body=addstory_form.body.data , img = filename )
-                db.session.add(new_story)
-                
-                
-                new_story_obj = Story.query.filter_by(title = story_title).first()
-                if(new_story_obj):
-                    new_story_obj.uploaded_by = current_user.id
-                    db.session.commit()
-                    
-                    return redirect(url_for('story_page'))
-            else:
-                new_story = Story(title = addstory_form.title.data , category = addstory_form.category.data, description = addstory_form.description.data, body=addstory_form.body.data )
-                db.session.add(new_story)
-                
-            
-            
-                """_summary_  assigning ownership of each post to the user that uploaded it. using the story title to reference ownership for the user that posted it.
-
-                Args:
-                    None(_type_):instance of Add_storyForm() from POST request client .
-                    _description_ 
-                """
-                #new_story_title =  request.form.get('addstory')
-                new_story_obj = Story.query.filter_by(title = story_title).first()
-                if(new_story_obj):
-                    new_story_obj.uploaded_by = current_user.id
-                    db.session.commit()
-                    
-                    return redirect(url_for('story_page'))
-        
-            
-        
-        
+        Route_func.add_story(addstory_form)
+        flash('New Post Added', category='success')
+        return redirect(url_for('story_page')) 
+     
     return render_template('addstory.html', addstory_form=addstory_form , stories=stories)
     
     
